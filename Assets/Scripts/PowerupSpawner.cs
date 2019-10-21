a6c7d29f1fe9f83477e26bc2309e22892fc846a5
Roman Saykin <sc2aru@gmail.com>
2019-10-21 21:45:01 +0300

    deleted debugging shit

--- Assets/Scripts/PowerupSpawner.cs
+++ Assets/Scripts/PowerupSpawner.cs
@@ -15,11 +15,9 @@ public class PowerupSpawner : MonoBehaviour
     public void SpawnPowerUp(Vector3 enemyDiedPosition)
     {
         int chance = Random.Range(0, 101);
-        Debug.Log("Chance is: " + chance);
         if (chance <= _spawnChance)
         { 
            GameObject powerUp = Instantiate(_powerUp[ChoosePowerUp()], enemyDiedPosition, Quaternion.identity) as GameObject;
-            Debug.Log("SPAWNED");
         }
     }
 }
